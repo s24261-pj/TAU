1f7d1d4e312833b074981ab5057e897324ae610d
MateuszKopczynski <mateusz.kopczynski23@gmail.com>
2024-11-08 23:13:53 +0100

    Lab-2: Add E2E tests for rest websites

---
+++ lab-2/__tests__/fishingstore/fishingstore-safari.test.js
@@ -0,0 +1,78 @@
+const { Builder, By, Key, until} = require('selenium-webdriver');
+
+describe('fishingstore.pl', () => {
+  let driver;
+
+  beforeAll(async () => {
+
+    driver = await new Builder().forBrowser('safari').build();
+  });
+
+  afterAll(async () => {
+    await driver.quit();
+  });
+
+  test('should navigate through categories and search for products', async () => {
+    await driver.manage().window().setRect({width: 1280, height: 720})
+    await driver.get('https://www.fishingstore.pl/');
+    expect(await driver.getTitle()).toContain('Sklep Wędkarski - sprzęt dla wędkarza - FishingStore.pl');
+
+    const newProductsLink = await driver.findElement(By.linkText('Nowości'));
+    await newProductsLink.click();
+
+    const currentUrl = await driver.getCurrentUrl();
+    expect(await currentUrl).toContain('https://www.fishingstore.pl/Nowosc-snewproducts-pol.html');
+
+    const productList = await driver.findElement(By.css('.products'));
+    expect(await productList.isDisplayed()).toBe(true);
+
+    const quantityIncrease  = await driver.wait(
+       until.elementLocated(By.css('.products .product:first-child .nb_up')),
+       5000
+    );
+    await quantityIncrease.click();
+
+    const quantity = await driver.findElement(By.css('.prod_count input'));
+    expect(await quantity.getAttribute('value')).toBe('2');
+
+    const decreaseQuantityButton = await driver.findElement(By.css('.products .product:first-child .nb_down'));
+    await decreaseQuantityButton.click();
+
+    expect(await quantity.getAttribute('value')).toBe('1');
+
+    const favoriteButton  = await driver.wait(
+       until.elementLocated(By.css('.products .product:first-child .--list-shopping-list')),
+       5000
+    );
+    await favoriteButton.click();
+
+    const chooseModal = await driver.findElement(By.css('.sl_choose.--active'));
+    expect(await chooseModal.isDisplayed()).toBe(true);
+
+    const createListButton = await driver.findElement(By.linkText('Stwórz nową listę zakupową'));
+    await driver.executeScript("arguments[0].click();", createListButton);
+
+    const input = await driver.findElement(By.css('.sl_create__input'));
+    await input.sendKeys('test' , Key.RETURN);
+
+    const parentDiv = await driver.wait(
+       until.elementLocated(By.xpath("//div[contains(@class, 'sl_choose__list')]//span[@class='sl_choose__name' and text()='test']/ancestor::div[contains(@class, 'sl_choose__list')]")),
+       10000
+    );
+
+    const chooseElementButton = await parentDiv.findElement(By.css('.sl_choose__group_label.f-label.py-4'));
+    await driver.executeScript("arguments[0].click();", chooseElementButton);
+
+    const button = await driver.wait(
+       until.elementLocated(By.xpath(".//button[@class='sl_choose__button --desktop btn --solid']")),
+       10000
+    );
+    await driver.executeScript("arguments[0].click();", button);
+
+    await driver.sleep(1000);
+
+    const result = await parentDiv.findElement(By.css('.sl_choose__count'));
+    expect(await result.getText()).toBe("1");
+    expect(await result.getText()).not.toBeFalsy();
+  }, 30000);
+});
---
+++ lab-2/__tests__/koleo/koleo-chrome.test.js
@@ -0,0 +1,90 @@
+const { Builder, By, until } = require('selenium-webdriver');
+
+describe('koleo.pl', () => {
+  let driver;
+
+  beforeAll(async () => {
+    driver = await new Builder().forBrowser('chrome').build();
+  });
+
+  afterAll(async () => {
+    await driver.quit();
+  });
+
+  test('should open train information', async () => {
+    await driver.get('https://koleo.pl/');
+    expect(await driver.getTitle()).toContain('KOLEO');
+
+    const inputContainers = await driver.wait(
+       until.elementsLocated(By.css('.large-4.columns.has-input.query-form-element')),
+       4000
+    );
+
+    const values = ["Tczew", "Gdańsk"];
+
+    for (let i = 0; i < inputContainers.length; i++) {
+      const container = inputContainers[i];
+
+      const input = await container.findElement(By.css('input'));
+
+      if (i !== inputContainers.length - 2) {
+        await input.sendKeys(values[i]);
+
+        const ul = await container.findElement(By.css('ul.autocomplete'));
+        await driver.wait(until.elementIsVisible(ul), 5000);
+        const li = await container.findElement(By.css('li.active'));
+
+        await li.click();
+        await driver.sleep(500);
+        const value = await input.getAttribute('value');
+        expect(value).toContain(values[i]);
+      }
+
+      if (i === 2) break;
+    }
+
+    const button = await driver.findElement(By.id('small_search'));
+    const buttonDisable = await button.getAttribute('disabled');
+    expect(buttonDisable).toBeNull();
+
+    await button.click();
+
+    const startStation = await driver.wait(
+       until.elementLocated(By.css('.departure-header .icon-with-text strong')),
+       5000
+    );
+    const startStationText = await startStation.getText();
+    expect(startStationText.toLowerCase()).toBe(values[0].toLowerCase());
+
+    const finalStation = await driver.findElement(By.css('.arrival-header .icon-with-text strong'));
+    const finalStationText = await finalStation.getText();
+    expect(finalStationText.toLowerCase()).toBe(values[1].toLowerCase());
+
+    const searchContainer = driver.wait(
+       until.elementLocated(By.css('.search-results')),
+       5000
+    );
+
+    const firstElement = await searchContainer.findElement(By.css('.has-train-nr'));
+    await firstElement.click();
+
+    await driver.sleep(2000);
+
+    const link = await searchContainer.findElement(By.css('a.ember-view'));
+    expect(await link.getText()).toBe('Szczegóły połączenia')
+    await driver.executeScript("arguments[0].click();", link);
+
+    const buttonOpenTable = await driver.wait(
+       until.elementLocated(By.css('.brand-logo.brand-logo-REG.clickable')),
+       1000
+    );
+    await driver.executeScript("arguments[0].click();", buttonOpenTable);
+    const heading = await driver.wait(
+       until.elementLocated(By.css('.small-16.columns')),
+       1000
+    );
+    expect(await heading.getAttribute('outerHTML')).toContain(values[0]);
+    expect(await heading.getAttribute('outerHTML')).toContain(values[1]);
+
+  }, 10000);
+});
